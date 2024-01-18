acffcd897aa131edb687b27999b922dcddb1d56b
matthew kettelkamp <mattkettelkamp@gmail.com>
2024-01-17 19:22:16 -0500

    adjusted the movement of the light

--- src/Lights.jsx
+++ src/Lights.jsx
@@ -8,7 +8,9 @@ export default function Lights()
 
     const lightRef = useRef()
     useFrame((state, delta) => {
-      easing.dampE(lightRef.current.position, [(state.pointer.x * 0.6), (state.pointer.y * 0.7), 0], 0.2, delta)
+
+      const dampingFactor = 0.5;
+      easing.dampE(lightRef.current.position, [(state.pointer.x * 2.9), (state.pointer.y * 1.2), 0], dampingFactor, delta)
 
       // spin the light around with time slowly
       // lightRef.current.rotation.z += 0.1 * delta
@@ -17,7 +19,7 @@ export default function Lights()
     return <>
 
         <group ref={lightRef}>
-          <directionalLight castShadow position={[ -8, 0.5, 2 ]} intensity={5} shadow-mapSize={1024}>
+          <directionalLight castShadow position={[ -8, 1.5, 3 ]} intensity={5} shadow-mapSize={1024}>
             <orthographicCamera attach="shadow-camera" args={[-10, 10, -10, 10, 0.1, 50]} />
           </directionalLight>
           <pointLight  position={[-3.9, 0.7, 7 ]} power={100} decay={1.4} />
